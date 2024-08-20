374079fb32c7340df96e5a07d82856957f205bb6
Satyanshu kumar <skscience30601@gmail.com>
2024-08-20 08:24:09 +0000

    [major] added script for downloading ms marco dataset

---
+++ download_dataset.py
@@ -0,0 +1,19 @@
+'''Script to download the MS MARCO dataset'''
+import subprocess
+import logging
+from constants import DownloadMSMARCOConstants as const
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# Define the data directory
+DATA_DIR = const.DOWNLOAD_PATH
+
+# Create the directory if it doesn't exist
+logging.log(logging.INFO, "Creating directory %s...", DATA_DIR)
+subprocess.run(["mkdir", "-p", DATA_DIR], check=False)
+
+# Download each file
+logging.log(logging.INFO, "Downloading files...")
+for url in const.URLS:
+    subprocess.run(["wget", url, "-P", DATA_DIR], check=False)
+
