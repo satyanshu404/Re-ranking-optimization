8f35e18b1dea181ca7eb1dceaac764fa980e7146
Satyanshu kumar <skscience30601@gmail.com>
2024-08-28 16:46:05 +0000

    [minor] added constants for full mapping script

--- constants.py
+++ constants.py
@@ -6,16 +6,16 @@ from typing import List
 @dataclass
 class BertConstants:
     ''' Constants for BERT model '''
-    MODEL_CHECKPOINT = 'bert-base-uncased'
+    MODEL_CHECKPOINT = 'google-bert/bert-large-uncased'
     TEST_SIZE = 0.2
-    BATCH_SIZE = 16
+    BATCH_SIZE = 8
     LEARNING_RATE = 1e-5
-    EPOCHS = 5
+    EPOCHS = 10
     NUMBER_OF_CLASSES = 2
     DATASET_PATH = "data/training/dataset.tsv"
     SAVE_MODEL_DIR = "models/Bert"
     SAVE_TOKENIZER_DIR = "models/Bert"
-    SAVE_MODEL_PATH = "bert-base-uncased-finetuned-v0.3"
+    SAVE_MODEL_PATH = "bert-large-uncased-finetuned-v0.2"
 
 @dataclass
 class CreateDatasetConstants:
@@ -29,11 +29,20 @@ class DownloadMSMARCOConstants:
     ''' Constants for downloading MSMARCO dataset '''
     DOWNLOAD_PATH = "data/MSMARCO"
     URLS: List = field(default_factory=lambda: [
-        "https://msmarco.z22.web.core.windows.net/msmarcoranking/msmarco-doctrain-queries.tsv.gz",
+        # Docs
+        "https://msmarco.z22.web.core.windows.net/msmarcoranking/msmarco-docs.tsv.gz",
         "https://msmarco.z22.web.core.windows.net/msmarcoranking/msmarco-docs-lookup.tsv.gz",
+
+        # Train dataset
+        "https://msmarco.z22.web.core.windows.net/msmarcoranking/msmarco-doctrain-queries.tsv.gz",
         "https://msmarco.z22.web.core.windows.net/msmarcoranking/msmarco-doctrain-qrels.tsv.gz",
         "https://msmarco.z22.web.core.windows.net/msmarcoranking/msmarco-doctrain-top100.gz",
-        "https://msmarco.z22.web.core.windows.net/msmarcoranking/msmarco-docs.tsv.gz"])
+        
+        # Dev dataset
+        "https://msmarco.z22.web.core.windows.net/msmarcoranking/msmarco-test2019-queries.tsv.gz",
+        "https://msmarco.z22.web.core.windows.net/msmarcoranking/msmarco-docdev-qrels.tsv.gz",
+        "https://msmarco.z22.web.core.windows.net/msmarcoranking/msmarco-doctest2019-top100.gz"
+    ])
 
 @dataclass
 class MapMSMARCOConstants:
@@ -46,3 +55,13 @@ class MapMSMARCOConstants:
     SAVE_PATH = "data/MSMARCO/triples_10k.tsv"
     NUMBER_OF_TRIPLES = 10000
     TRIPLET_COMPLETED = 1000
+
+@dataclass
+class MapAllMSMARCOConstants:
+    ''' Constants for MAP@MSMARCO '''
+    DOCS_PATH = "data/MSMARCO/msmarco-docs.tsv.gz"
+    DOCS_LOOKUP_PATH = "data/MSMARCO/msmarco-docs-lookup.tsv.gz"
+    QUERIES_PATH = "data/MSMARCO/msmarco-test2019-queries.tsv.gz"
+    DOCDEV_QRELS_PATH = "data/MSMARCO/msmarco-docdev-qrels.tsv.gz"
+    DOCDEV_TOP100_PATH = "data/MSMARCO/msmarco-doctest2019-top100.gz"
+    SAVE_PATH = "data/MSMARCO/dev_all_mapped_t10.tsv"
