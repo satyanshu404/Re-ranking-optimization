a1f080e5a5ddebaa095d85e1b5933dbd7c010b23
Satyanshu kumar <skscience30601@gmail.com>
2024-10-01 06:08:26 +0000

    [major] updated the code for handling larger dataset

--- finetune_bert.py
+++ finetune_bert.py
@@ -8,24 +8,24 @@ Change the hyperparameters in the BertConstants class from constants.py file.
 import os
 import logging
 import torch
+from tqdm import tqdm 
 import pandas as pd
 from constants import BertConstants as const
 from losses import CrossEntropyLoss
 from sklearn.model_selection import train_test_split
 from transformers import BertForSequenceClassification, AutoTokenizer
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import DataLoader, IterableDataset, Dataset
 from torch.optim import AdamW
 
+os.environ["CUDA_VISIBLE_DEVICES"] = const.VISIBLE_DEVICES
+# os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
 # Configure logging
-logging.basicConfig(level=logging.INFO)
-# logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 class TokenData(Dataset):
     '''Dataset class for tokenized data'''
-    def __init__(self,
-                 train_x: pd.DataFrame,
-                 train_y: pd.DataFrame,
-                 train_tokens: dict) -> None:
+    def __init__(self, train_x: pd.DataFrame, train_y: pd.DataFrame, train_tokens: dict) -> None:
         '''Initialize the dataset'''
         self.text_data = train_x
         self.tokens = train_tokens
@@ -35,11 +35,32 @@ class TokenData(Dataset):
         return len(self.text_data)
 
     def __getitem__(self, idx):
-        sample = {}
-        for k, v in self.tokens.items():
-            sample[k] = torch.tensor(v[idx])
+        sample = {k: torch.tensor(v[idx]) for k, v in self.tokens.items()}
         sample['labels'] = torch.tensor(self.labels[idx])
         return sample
+
+class TokenIterableData(IterableDataset): # pylint: disable = abstract-method
+    '''IterableDataset for large datasets'''
+    def __init__(self, data, labels, tokenizer, batch_size=const.BATCH_SIZE):
+        '''Initialize the class'''
+        self.data = data
+        self.labels = labels
+        self.tokenizer = tokenizer
+        self.batch_size = batch_size
+
+    def preprocess_batch(self, start_idx, end_idx):
+        '''Preprocess the batch'''
+        batch_data = list(self.data[start_idx:end_idx])
+        tokens = self.tokenizer(batch_data, padding=True, truncation=True, return_tensors="pt")
+        labels = torch.tensor(self.labels[start_idx:end_idx])
+        tokens['labels'] = labels
+        return tokens
+
+    def __iter__(self):
+        '''Iterate over the data'''
+        for start_idx in range(0, len(self.data), self.batch_size):
+            end_idx = min(start_idx + self.batch_size, len(self.data))
+            yield self.preprocess_batch(start_idx, end_idx)
 
 class Bert:
     """BERT model for sequence classification"""
@@ -48,6 +69,7 @@ class Bert:
                  device: torch.device="cpu") -> None:
         """Initialize the BERT model"""
         self.device = device
+        self.seed = 0
         self.model = BertForSequenceClassification.from_pretrained(model_checkpoint,
                                                                    num_labels=num_labels)
         self.tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
@@ -55,82 +77,107 @@ class Bert:
         self.model.to(self.device)
 
         # loggings
-        logging.log(logging.INFO, "MODEL INFORMATION: %s", '='*50)
-        logging.log(logging.INFO, "Model and tokenizer loaded from: %s...", model_checkpoint)
-        logging.log(logging.INFO, "Number of classes set to: %s...", num_labels)
-        logging.log(logging.INFO, "Optimizer set to: %s...", self.optimizer.__class__.__name__)
-        logging.log(logging.INFO, "Batch size set to: %s...", const.BATCH_SIZE)
-        logging.log(logging.INFO, "Learning rate set to: %s...", const.LEARNING_RATE)
-        logging.log(logging.INFO, "Device set to: %s...", self.device)
-        logging.log(logging.INFO, "%s", '='*69)
-    
+        logging.info("MODEL INFORMATION: %s", '='*50)
+        logging.info("Model and tokenizer loaded from: %s...", model_checkpoint)
+        logging.info("Number of classes set to: %s...", num_labels)
+        logging.info("Optimizer set to: %s...", self.optimizer.__class__.__name__)
+        logging.info("Batch size set to: %s...", const.BATCH_SIZE)
+        logging.info("Learning rate set to: %s...", const.LEARNING_RATE)
+        logging.info("Visible device set to: %s...", const.VISIBLE_DEVICES)
+        logging.info("Device set to: %s...", self.device)
+        logging.info("Random seed set to: %s...", self.seed)
+        logging.info("%s", '='*69)
+
     def read_data(self, data_path: str) -> pd.DataFrame:
         '''Read the data from the given tsv path only'''
         assert data_path.endswith(".tsv"), "Only TSV files are supported..."
+        logging.info("Reading the data...")
         data = pd.read_csv(data_path, sep="\t")
-        logging.log(logging.INFO, "Data read successfully...")
+        logging.info("Data shape: %s...", data.shape)
         return data
     
+    def batch_tokenize(self, data: list, batch_size: int) -> dict:
+        '''Tokenize the data in batches'''
+        logging.info("Tokenizing the data in batches of %s...", batch_size)
+        tokens = {"input_ids": [], "attention_mask": []}
+        for i in tqdm(range(0, len(data), batch_size), desc="Tokenizing dataset: ",unit=" batch", total=len(data)//batch_size):
+            batch_data = data[i:i + batch_size]
+            batch_tokens = self.tokenizer(list(batch_data), padding=True, truncation=True)
+            tokens["input_ids"].extend(batch_tokens["input_ids"])
+            tokens["attention_mask"].extend(batch_tokens["attention_mask"])
+        return tokens
+
     def prepare_data(self,
                      data: pd.DataFrame,
                      labels: pd.DataFrame,
-                     test_size: float=const.TEST_SIZE) -> tuple:
-        '''Prepare the data for training and testing'''
-        x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=test_size)
+                     test_size: float=const.TEST_SIZE,
+                     batch_size: int=const.BATCH_SIZE,
+                     num_workers: int=const.NUM_WORKERS) -> tuple:
+        '''Prepare data using IterableDataset for large datasets'''
+        logging.info("Preparing dataset for training...")
 
-        train_tokens = self.tokenizer(list(x_train), padding = True, truncation=True)
-        test_tokens = self.tokenizer(list(x_test), padding = True, truncation=True)
+        # Split dataset
+        x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=test_size, random_state=self.seed)
+        logging.info("Train data shape: %s, Test data shape: %s", x_train.shape, x_test.shape)
 
+        # Tokenize train and test data in batches
+        logging.info("Tokenizing the data...")
+        train_tokens = self.batch_tokenize(list(x_train), batch_size=10000)
+        test_tokens = self.batch_tokenize(list(x_test), batch_size=10000)
+
+        # Use TokenData for datasets
+        logging.info("Creating DataLoader for training and testing...")
         train_dataset = TokenData(x_train, y_train, train_tokens)
-        train_loader = DataLoader(train_dataset, shuffle=True, batch_size=const.BATCH_SIZE)
-
         test_dataset = TokenData(x_test, y_test, test_tokens)
-        test_loader = DataLoader(test_dataset, shuffle=True, batch_size=const.BATCH_SIZE)
 
-        logging.log(logging.INFO, "Train and test data prepared successfully...")
+        # DataLoader with optimization
+        train_loader = DataLoader(train_dataset, shuffle=True, batch_size=batch_size, 
+                                pin_memory=True, num_workers=num_workers)
+        test_loader = DataLoader(test_dataset, shuffle=False, batch_size=batch_size, 
+                                pin_memory=True, num_workers=num_workers)
+
+        logging.log(logging.INFO, "Train loader batch size: %s, dataset size: %s",
+                    train_loader.batch_size, len(train_loader.dataset))
+        logging.log(logging.INFO, "Test loader batch size: %s, dataset size: %s",
+                    test_loader.batch_size, len(test_loader.dataset))
+
         return train_loader, test_loader
-    
-    def evaluator(self,
-                  test_loader: DataLoader,
-                  loss_fn) -> None:
+
+    def evaluator(self, test_loader: DataLoader, loss_fn) -> None:
         '''Evaluate the model'''
         self.model.eval()
         
         total_loss = 0.0
         correct = 0
-        total_examples = 0
 
         with torch.no_grad():
-            for batch in test_loader:
+            for batch in tqdm(test_loader, desc="Evaluating", leave=False, total= len(test_loader.dataset) // test_loader.batch_size):
                 input_ids = batch['input_ids'].to(self.device)
                 attention_mask = batch['attention_mask'].to(self.device)
                 labels = batch['labels'].to(self.device)
-                
+
                 outputs = self.model(input_ids, attention_mask=attention_mask)
                 pred = outputs.logits
                 loss = loss_fn(pred, labels)
 
                 total_loss += loss.item()
                 correct += (pred.argmax(1) == labels).sum().item()
-                total_examples += labels.size(0)
-            average_loss = total_loss / len(test_loader)
-            accuracy = correct / total_examples
-            logging.log(logging.INFO, "Test loss: %s", average_loss)
-            logging.log(logging.INFO, "Testing accuracy: %s", accuracy)
 
-    
-    def trainer(self,
-                train_loader: DataLoader,
-                test_loader: DataLoader,
-                epochs: int,
-                loss_fn) -> None:
+            average_loss = total_loss / len(test_loader)
+            accuracy = correct / len(test_loader.dataset)
+            logging.info("Test loss: %s, Testing accuracy: %s", average_loss, accuracy)
+
+
+    def trainer(self, train_loader: DataLoader, test_loader: DataLoader, epochs: int, loss_fn) -> None:
         '''Train the model'''
+        logging.info("Training the model...")
         for epoch in range(epochs):
             self.model.train()
-            logging.log(logging.INFO, "Epoch %d: %s", epoch + 1, '='*50)
+            logging.info("Epoch %d: %s", epoch + 1, '='*50)
             total_train_loss = 0.0
+            correct = 0
             
-            for batch in train_loader:
+            for batch in tqdm(train_loader, desc="Training", leave=False, total= len(train_loader.dataset) // train_loader.batch_size):
                 self.optimizer.zero_grad()
                 input_ids = batch['input_ids'].to(self.device)
                 attention_mask = batch['attention_mask'].to(self.device)
@@ -143,17 +190,17 @@ class Bert:
                 loss.backward()
                 self.optimizer.step()
                 total_train_loss += loss.item()
+                correct += (pred.argmax(1) == labels).sum().item()
+            
             average_train_loss = total_train_loss / len(train_loader)
-            logging.log(logging.INFO, "Training loss: %s", average_train_loss)
+            accuracy = correct / len(train_loader.dataset)
+            logging.info("Train loss: %s, Train accuracy: %s", average_train_loss, accuracy)
 
             # Call the evaluator function for the test set
             self.evaluator(test_loader, loss_fn)
 
 
-    def train(self,
-              file_path: str,
-              loss_function,
-              epochs: int=const.EPOCHS) -> None:
+    def train(self, file_path: str, loss_function, epochs: int=const.EPOCHS) -> None:
         '''Train the model'''
         # Read the data
         data = self.read_data(file_path)
@@ -167,18 +214,18 @@ class Bert:
         
         # Train the model
         self.trainer(train_loader, test_loader, epochs, loss_function)
-        logging.log(logging.INFO, "Training completed...")
+        logging.info("Training completed...")
         
-        # check if the directory exists
+        # Check if the directory exists
         if not os.path.exists(const.SAVE_MODEL_DIR):
-            logging.log(logging.INFO, "No existing directory '%s' found, creating one...", const.SAVE_MODEL_DIR)
+            logging.info("No existing directory '%s' found, creating one...", const.SAVE_MODEL_DIR)
             os.makedirs(const.SAVE_MODEL_DIR)
 
         # Save the model and tokenizer
         save_path: str = os.path.join(const.SAVE_MODEL_DIR, const.SAVE_MODEL_PATH)
         self.model.save_pretrained(save_path)
         self.tokenizer.save_pretrained(save_path)
-        logging.log(logging.INFO, "Model and tokenizer saved successfully at %s...", const.SAVE_MODEL_DIR)
+        logging.info("Model and tokenizer saved successfully at %s...", const.SAVE_MODEL_DIR)
 
 if __name__ == "__main__":
     # variables
